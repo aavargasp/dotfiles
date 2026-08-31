8cf6d46fdfc5e1936372382b20381e2c006aa24d
Andres Vargas <avargasp87@gmail.com>
2026-08-30 21:27:11 -0400

    added hyprland and noctalia configs

---
+++ dot_config/hypr/config/monitors.lua
@@ -0,0 +1,20 @@
+hl.monitor({
+    output = MONITOR1,
+    mode = "1920x1200@240.01",
+    position = "4880x240",
+    scale = 1.0
+})
+hl.monitor({
+    output = MONITOR2,
+    mode = "3440x1440@174.96",
+    position = "1440x0",
+    scale = 1,
+    bitdepth = 10
+})
+hl.monitor({
+    output = MONITOR3,
+    mode = "2560x1440@59.95",
+    position = "0x0",
+    scale = 1,
+    transform = 1
+})
---
+++ dot_config/hypr/config/variables.lua
@@ -0,0 +1,16 @@
+-- Hyprland default apps
+
+TERMINAL     = "kitty"
+FILE_MANAGER = "dolphin"
+BROWSER      = "brave"
+EDITOR       = "gnome-text-editor --new-window"
+CALCULATOR   = "gnome-calculator"
+
+-- Monitors
+MONITOR1 = "eDP-1"
+MONITOR2 = "DP-3"
+MONITOR3 = "DP-2"
+PRIMARY_MONITOR = MONITOR2
+
+-- Workspaces
+NUM_WPM = 3 -- Number of workspaces per monitor (Max 10)
